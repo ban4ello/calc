48fba64b845b278937bcbd12bd1492fdbb8d961b
ban4ello <ban4ellog@gmail.com>
2019-10-22 15:47:55 +0300

    init commit

---
+++ src/components/calculator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="calculator">
+    calculator
+  </div>
+</template>
+
+<script>
+export default {
+  name: "calculator",
+  props: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.calculator {
+  width: 100px;
+  height: 100px;
+}
+</style>
